d207017bcce6405ee3df6ee5b787826709e92606
SellesOG <adselles@MacBook-Air-de-Vanessa.local>
2025-10-21 02:12:47 +0200

    bucles

---
+++ Proyectos/for.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int main (int argc, char *argv[])
+{
+   int n = 0;
+
+   printf("Argumentos recibidos\n");
+
+   for(n = 0; n < argc; n++)
+   {
+      printf("Num. %d contendido=%s\n", n, argv[n]);
+   }
+   return 0;
+}
