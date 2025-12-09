582756d72d8bbed0569e494afcc0e2666c914666
Adrian Selles García <adrisega311203@gmail.com>
2025-12-09 22:14:51 +0100

    calculadorda del radio de un circulo

---
+++ Pruebas/calc_2.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <math.h>
+
+int main()
+{
+  double radius = 0.0;
+  double area = 0.0;
+  const double PI = 3.14159;
+
+  printf("Enter the radius: ");
+  scanf("%lf", &radius);
+
+  area = PI * radius * radius;
+
+  printf("Area: %.2lf", area);
+
+  return 0;
+}
