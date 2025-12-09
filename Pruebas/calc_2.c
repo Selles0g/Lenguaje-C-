2327cd22b4b480600415818cd74173f07dd1ecf9
Adrian Selles García <adrisega311203@gmail.com>
2025-12-09 22:18:38 +0100

    renombre de variables

--- Pruebas/calc_2.c
+++ Pruebas/calc_2.c
@@ -3,14 +3,14 @@
 
 int main()
 {
-  double radius = 0.0;
+  double radio = 0.0;
   double area = 0.0;
   const double PI = 3.14159;
 
   printf("Enter the radius: ");
-  scanf("%lf", &radius);
+  scanf("%lf", &radio);
 
-  area = PI * radius * radius;
+  area = PI * radio * radio;
 
   printf("Area: %.2lf", area);
 
